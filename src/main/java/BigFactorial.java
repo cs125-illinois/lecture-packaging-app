4617a74d7856febf35794d8ca79e465753599ab0
Geoffrey Challen <challen@illinois.edu>
2018-04-15 15:57:48 -0500

    Solution for Google example.

--- src/main/java/BigFactorial.java
+++ src/main/java/BigFactorial.java
@@ -1,8 +1,10 @@
+import com.google.common.math.BigIntegerMath;
+
 /**
  * A class that prints a big factorial.
  */
 public class BigFactorial {
     public static void main(final String[] unused) {
-        System.out.println("Here");
+        System.out.println(BigIntegerMath.factorial(100));
     }
 }

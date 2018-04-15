3a1ded91792fac1d82c2e5f58e8033a68c8d8f3a
Geoffrey Challen <challen@illinois.edu>
2018-04-15 17:11:49 -0500

    Adding jitpack solution.

--- src/main/java/BigFactorial.java
+++ src/main/java/BigFactorial.java
@@ -1,10 +1,15 @@
 import com.google.common.math.BigIntegerMath;
+import edu.illinois.cs.cs125.sortlib.Sorting;
+import java.util.Arrays;
 
 /**
  * A class that prints a big factorial.
  */
 public class BigFactorial {
     public static void main(final String[] unused) {
+        int[] inputArray = new int[] { 1, 3, 2 };
+        Sorting.mergeSort(inputArray);
+        System.out.println(Arrays.toString(inputArray));
         System.out.println(BigIntegerMath.factorial(100));
     }
 }
